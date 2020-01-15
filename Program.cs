9e97d786dc074712a63104f56b71b262315c59aa
Dan Rasmussen <drasmussen49@gmail.com>
2020-01-15 12:59:30 -0500

    First commit for this project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PracticeInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = 4;
+            int costPerPerson = 13;
+            int totalBill = numberOfPeople * costPerPerson;
+            Console.WriteLine(totalBill);
+            if (totalBill >= 50)
+            {
+                double discount = totalBill * .1;
+                Console.WriteLine(discount);
+            }
+        }
+    }
+}
